99b78d174d28bd372697dad0ba96fd83d4dfdfb7
GabrielRGA <rodriguesgomesgabriel@hotmail.com>
2023-10-19 17:47:57 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERROD ANUNCIADO",
+    "description":"Marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://is1-ssl.mzstatic.com/image/thumb/ZypiNEdbU0wwCF0GMJ3zoA/1200x675mf.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Nova Série anunciada no Disney+",
+    "description":"bla bla bla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+]
